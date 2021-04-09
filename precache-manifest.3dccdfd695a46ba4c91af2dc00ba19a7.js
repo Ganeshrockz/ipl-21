eb9b96ba2250db3e1ac837a1d94f9564a87958a6
Ganeshrockz <ganeshrockz@github.com>
2021-04-10 00:33:18 +0530

    Updates

---
+++ precache-manifest.3dccdfd695a46ba4c91af2dc00ba19a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edc294a5a5d857112126",
+    "url": "/ipl-21/static/css/main.31e8e36b.chunk.css"
+  },
+  {
+    "revision": "edc294a5a5d857112126",
+    "url": "/ipl-21/static/js/main.edc294a5.chunk.js"
+  },
+  {
+    "revision": "ebf5cdb8e34f552d05c1",
+    "url": "/ipl-21/static/js/runtime~main.ebf5cdb8.js"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/ipl-21/static/css/2.801b82c9.chunk.css"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/ipl-21/static/js/2.0be92d95.chunk.js"
+  },
+  {
+    "revision": "200e2cafa37f45375eb394e507166a39",
+    "url": "/ipl-21/index.html"
+  }
+];
